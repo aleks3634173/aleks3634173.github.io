b53722c75c7ce70e67561795ee70a3eaa1e9e92c
aleks <aleks3634173@gmail.com>
2021-01-02 20:05:11 +0300

    update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,7 +5,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 $(document).ready(function() {
-  
+  // fix 100vh
+  updateHeight()
+  function updateHeight() {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  // fix 100vw
+  updateWidth()
+  function updateWidth() {
+    var vw = window.innerWidth-$(document).width();
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+  }
   
 })
 
